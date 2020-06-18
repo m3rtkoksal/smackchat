f0474b610cee873987308423aa849181bf0654c6
MK <mertkoksal88@gmail.com>
2020-06-18 13:06:19 +0300

    Initial Commit

---
+++ SmackChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmackChat
+//
+//  Created by Mert Köksal on 18.06.2020.
+//  Copyright © 2020 Mert Köksal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
