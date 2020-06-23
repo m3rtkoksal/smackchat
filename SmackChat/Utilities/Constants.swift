5229c50a61bf748085c75c3c0283fb8112475420
MK <mertkoksal88@gmail.com>
2020-06-23 14:43:00 +0300

    heroku and userdefaults are set

--- SmackChat/Utilities/Constants.swift
+++ SmackChat/Utilities/Constants.swift
@@ -8,6 +8,19 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+//URL Constants
+let BASE_URL = "https://chatsmackchatslack.herokuapp.com/v1"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
+//segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+
+//USER DEFAULT
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
