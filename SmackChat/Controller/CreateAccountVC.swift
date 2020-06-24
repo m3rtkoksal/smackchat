a97bc99d59dd17eb370a7091004d18d31e1d0557
MK <mertkoksal88@gmail.com>
2020-06-24 15:08:37 +0300

    user created and loged in with swiftyjson

--- SmackChat/Controller/CreateAccountVC.swift
+++ SmackChat/Controller/CreateAccountVC.swift
@@ -37,7 +37,12 @@ class CreateAccountVC: UIViewController {
         
         AuthService.instance.registerUser(email: email, password: password) { (success) in
             if success {
-                print("registered user")
+                //print("registered user")
+                AuthService.instance.loginUser(email: email, password: password) { (sucess) in
+                    if success {
+                        print("logged in user", AuthService.instance.authToken)
+                    }
+                }
             } 
         }
         
